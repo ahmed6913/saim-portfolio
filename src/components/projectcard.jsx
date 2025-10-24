e3b04b645f8f469928aa2245e6964d7b84d9aa68
ahmed6913 <ahmedshaikh777121@gnmail.com>
2025-10-24 14:37:32 +0300

    merge con. solve

--- src/components/projectcard.jsx
+++ src/components/projectcard.jsx
@@ -16,7 +16,7 @@ function ProjectCard() {
       title: "NFT Marketplace with admin page",
       description:
         "A modern Point of Sale (POS) system with NFT-based customer rewards and an integrated NFT Marketplace.",
-      technologies: ["Next.js", "React","TailwindCSS", "Go", "MongoDB", "MySQL", "Firebase"],
+      technologies: ["Next.js", "React", "TailwindCSS", "Go", "MongoDB", "MySQL", "Firebase"],
       liveLink: "https://nft-marketplace-81f39.web.app/",
       githubLink: "https://github.com/ahmed6913/NFT-Marketplace.git",
       image: "/NFT.png",
@@ -26,17 +26,17 @@ function ProjectCard() {
       title: "POS with admin page",
       description:
         "A modern Point of Sale (POS) system.",
-      technologies: ["Next.js", "React","TailwindCSS", "Go", "MongoDB", "MySQL", "Firebase","APIs","AWS"],
+      technologies: ["Next.js", "React", "TailwindCSS", "Go", "MongoDB", "MySQL", "Firebase", "APIs", "AWS"],
       liveLink: "https://web-app-pos-1deda.web.app/",
       githubLink: "https://github.com/ahmed6913/web-app-pos.git",
       image: "/pos.png",
     },
-     {
+    {
       id: 3,
       title: "DeFi Staking App (Decentralized Finance Application)",
       description:
         "A DeFi stack app (Decentralized Finance stack application) is a full-stack decentralized application that enables users to interact with financial services on the blockchain—without intermediaries.",
-      technologies: [ "React","TailwindCSS", "Go","Ether.js","Typescript", "Pinata", "Firebase","APIs"],
+      technologies: ["React", "TailwindCSS", "Go", "Ether.js", "Typescript", "Pinata", "Firebase", "APIs"],
       liveLink: "https://ahmed6913.github.io/DeFi-staking-app/",
       githubLink: "https://github.com/ahmed6913/DeFi-staking-app.git",
       image: "/defi.png",
@@ -46,17 +46,17 @@ function ProjectCard() {
       title: "Cubiclabs official website",
       description:
         "A service based startup landing page",
-      technologies: [ "Nextjs","TailwindCSS", "Firebase","APIs"],
+      technologies: ["Nextjs", "TailwindCSS", "Firebase", "APIs"],
       liveLink: "https://cubiclabs.vercel.app/",
       githubLink: "https://github.com/saim-cubiclabs/cubiclabs.git",
       image: "/cb.png",
     },
-     {
+    {
       id: 5,
       title: "Lawyer Appointment Booking System",
       description:
         "Clients can book appointments with lawyers and lawyers can manage their appointments.",
-      technologies: [ "Nextjs","TailwindCSS", "Firebase","FormspreeAPI"],
+      technologies: ["Nextjs", "TailwindCSS", "Firebase", "FormspreeAPI"],
       liveLink: "https://leqal-ack6.vercel.app/",
       githubLink: "https://github.com/saim-cubiclabs/leqal.git",
       image: "/leqal.png",
@@ -65,17 +65,10 @@ function ProjectCard() {
       id: 6,
       title: "Reward Platform",
       description:
-"A platform where users earn points by watching, liking, and sharing brand videos (Reels-style). Points can be redeemed for brand-specific rewards/coupons. Brands can upload campaigns, allocate points per engagement, and track basic analytics.",
-        
-<<<<<<< HEAD
-      technologies: [ "Nextjs","TailwindCSS", "Firebase"],
+        "A platform where users earn points by watching, liking, and sharing brand videos (Reels-style). Points can be redeemed for brand-specific rewards/coupons. Brands can upload campaigns, allocate points per engagement, and track basic analytics.",
+      technologies: ["Nextjs", "TailwindCSS", "Firebase"],
       liveLink: "https://reward-platform.vercel.app/",
       githubLink: "https://github.com/ahmed6913/reward-platform.git",
-=======
-      technologies: [ "Nextjs","TailwindCSS", "Firebase",],
-      liveLink: "",
-      githubLink: "https://github.com/ahmed6913/reward-platform",
->>>>>>> e1c2bf27865bb1e1e21b7f19af3d30580b373510
       image: "/reward.png",
     },
 
@@ -88,14 +81,13 @@ function ProjectCard() {
         <h2 className="text-4xl font-bold text-center text-gray-900 mb-12 pt-7">
           My <span className="text-blue-600 dark:text-blue-400">Projects (visit my <a href="https://github.com/ahmed6913" target="_blank" rel="noopener noreferrer" className="underline hover:text-blue-800">github</a> for more)</span>
         </h2>
-      
+
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
           {projects.map((project) => (
             <div
               key={project.id}
-              className={`transition-all duration-700 transform ${
-                isVisible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"
-              } bg-white text-black p-6 rounded-xl shadow-md`}
+              className={`transition-all duration-700 transform ${isVisible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"
+                } bg-white text-black p-6 rounded-xl shadow-md`}
             >
               <Image
                 src={project.image}
